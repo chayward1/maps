0fdc7f59b8aa4c8a7159e219591ab55f76a9fdda
Christopher James Hayward <chris@chrishayward.xyz>
2021-02-19 20:19:47 -0500

    Cleanup

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-const mapOptions = {
-    zoomControl: false,
-    attributionControl: false,
-};
-
-const map = L.map('map', mapOptions);
-
-const tileOptions = {
-    subdomains: 'abcd',
-    maxZoom: 16,
-    noWrap: true
-};
-
-const baseTileDark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', tileOptions);
-const baseTileLight = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', tileOptions);
-
-const baseTiles = {
-    "Dark": baseTileDark,
-    "Light": baseTileLight
-};
-
-const baseTileControls = L.control.layers(baseTiles);
-
+// Initialize the page.
 function init() {
+    const map = initMap('map');
+
     map.fitWorld();
     map.setZoom(4);
-    baseTileDark.addTo(map);
-    baseTileControls.addTo(map);
-    routingControls.addTo(map);
+    
+    const tiles = initTiles();
+    const controls = initControls(tiles);
+
+    tiles["Dark"].addTo(map);
+    controls.addTo(map);
 }
 
+// Initialize the map.
+function initMap(id) {
+    return L.map(id, {
+	zoomControl: false,
+	attributionControl: false,
+    });
+}
+
+// Initialize the map tiles.
+function initTiles() {
+    const opts = {
+	subdomains: 'abcd',
+	maxZoom: 16,
+	noWrap: true,
+    };
+
+    return {
+	"Dark": L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', opts),
+	"Light": L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', opts),
+    };
+}
+
+// Initialize the map controls.
+function initControls(tiles) {
+    return L.control.layers(tiles);
+}
+
+// Read URL query parameter(s).
+function arg(name) {
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+	return decodeURIComponent(name[1]);
+}
