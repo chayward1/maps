568e0f3f0b08840819d8aa17877844551d4f7e92
Christopher James Hayward <chris@chrishayward.xyz>
2021-02-21 15:03:08 -0500

    No declarations in app.js

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 window.onload = function() {
-    // Initialize the map.
-    const map =  L.map('map', {
+    L.map('map', {
 	zoomControl: false,
 	attributionControl: false,
-    });
-
-    // Initialize the layers.
-    const layers = {
-	"Dark": L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+	layers: L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
 	    subdomains: 'abcd',
 	    maxZoom: 16,
 	    noWrap: true,
-	}),
-	"Light": L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
-	    subdomains: 'abcd',
-	    maxZoom: 16,
-	    noWrap: true,
-	}),
-    };
-
-    // Compose the map.
-    map
+	})
+    })
 	.fitWorld()
 	.setZoom(4)
-	.addLayer(layers["Dark"])
-	.addControl(L.control.layers(layers));
+	.eachLayer(l => l.addTo(this));
 };
